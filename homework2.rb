2bf86318ac47cd71e3bd143cb262ceffec3817c5
AntonCherkago <bigman1313@rambler.ru>
2016-05-18 16:02:47 +0300

    1

---
+++ homework2.rb
@@ -0,0 +1,3 @@
+my_hash{"yes"=>23, "b"=>"travel", "yesterday"=>34, 5=>"234", :yesss=>:fg,
+try: 30, key: "some value", "yesterday"=>34, "yesteryear"=>2014}
+my_hash. find_all{ |key|, puts key.scan (/yes/)}
