afa2f0fc3239f7980788cc33d0ec8ac7acbaeb6d
AntonCherkago <bigman1313@rambler.ru>
2016-05-23 21:00:18 +0300

    12

---
+++ syntax_fix - копия.rb
@@ -0,0 +1,33 @@
+class Hometask1
+def initialize(array_task)
+  h = w = 4
+  @arr = Array.new(h){ Array.new(w){ rand(-10..10) } }
+  puts "Original array: #{arr}"
+  end
+  def sum
+  @arr.flatten!.select!{ |n| n < 0 }
+  puts "Negative numbers #{arr} Sum: #{arr.inject(:+)}"
+  end
+  end
+
+
+  require "minitest/autorun"
+  class TestHometask1 < Minitest::Test
+    def setup
+      @hometask1 = Hometask1.new
+    end
+
+    def test_rand
+      h = w = 4
+      @arr = Array.new(h){ Array.new(w){ rand(-10..10) } }
+      puts "Original array: #{arr}"
+      hometask1.stub(:rand, -1) do
+      assert_equal(-1, hometask1.flip)
+    end
+
+    def test_sum
+      @arr.flatten!.select!{ |n| n < 0 }
+      puts "Negative numbers #{arr} Sum: #{arr.inject(:+)}"
+      assert_equal(-16, hometask1.flip)
+    end
+  end
