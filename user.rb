233023bf6959b9b45907f79631fc1ef68fc70ea8
AntonCherkago <bigman1313@rambler.ru>
2016-06-19 14:04:18 +0300

    1

--- user.rb
+++ user.rb
@@ -3,7 +3,13 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates_date :date_of_birth, :before => lambda { 15.years.ago },
-                                 :before_message => "must be at least 15 years old"   #установлен гем validate_timeliness#
+                                 :before_message => "must be at least 15 years old"   #установлен гем validate_timeliness"
 
+  scope :by_status_and_age, -> status { where(status: active) }
+
+  def self.search(search)
+   where ( "first_name?" ,   "%#{search}%" )
+   where ( "last_name?" ,   "%#{search}%" ) 
+ end
 
 end
